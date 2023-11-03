c45e42a9b0b4dcae93d6820dbd6a0f551d592380
kot_eugene <to_ev@mail.ru>
2023-11-03 17:53:54 +0300

    Added User model

--- src/db_run.py
+++ src/db_run.py
@@ -1,17 +1,21 @@
 from db import db
 from app import app
-from models import Doctor
+from models import Doctor, User
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
 with app.app_context():
-    doctor = Doctor(first_name="Alexandr", second_name="Ivanov", age=47)
+    user = User(email="abc@mail.ru", first_name="Alexandr", second_name="Ivanov", age=47)
+    db.session.add(user)
+    db.session.commit()
+
+    doctor = Doctor(user=user, experience="5 months")
     db.session.add(doctor)
     db.session.commit()
 
-# with app.app_context():
-#     doctors = Doctor.query.all()
-#     print(doctors)
+with app.app_context():
+    doctors = Doctor.query.all()
+    print(doctors)
 
--- src/models.py
+++ src/models.py
@@ -1,10 +1,24 @@
 from db import db
 
-class Doctor(db.Model):
+class User(db.Model):
+    __tablename__ = "user"
+
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100))
     first_name = db.Column(db.String(100), nullable=False)
     second_name = db.Column(db.String(100))
     age = db.Column(db.Integer)
 
     def __repr__(self):
-        return f"<Doctor {self.first_name} {self.second_name}>"
+        return f"<User {self.first_name} {self.second_name}>"
+
+class Doctor(db.Model):
+    __tablename__ = "doctor"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.ForeignKey("user.id"), nullable=False)
+    user = db.Relationship("User")
+    experience = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f"<Doctor {self.user.first_name} {self.user.second_name}>"
